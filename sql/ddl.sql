60d8f09532d3d8f2fd82621799fbf166ae085ade
Mayank Thakur <mayankthakur3006@gmail.com>
2024-07-21 20:23:47 +0530

    added ddl sql file

---
+++ sql/ddl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE students (
+    id int,
+    roll_no varchar(10),
+    first_name varchar(100),
+    last_name varchar(100),
+	gender varchar(10),
+    admission_year date,
+    date_of_birth date,
+    email varchar(100),
+    phone_number int
+);
